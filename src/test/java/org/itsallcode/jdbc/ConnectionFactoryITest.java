f11a4fdd4400c87173f903e5156109a41f2bae86
kaklakariada <kaklakariada@users.noreply.github.com>
2024-01-20 18:18:14 +0100

    Improve test coverage

--- src/test/java/org/itsallcode/jdbc/ConnectionFactoryITest.java
+++ src/test/java/org/itsallcode/jdbc/ConnectionFactoryITest.java
@@ -1,6 +1,7 @@
 package org.itsallcode.jdbc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,5 +19,26 @@ class ConnectionFactoryITest {
         try (SimpleConnection connection = connectionFactory.create("jdbc:h2:mem:")) {
             assertThat(connection).isNotNull();
         }
+    }
+
+    @Test
+    void createConnectionFailsUnknownDbType() {
+        assertThatThrownBy(() -> connectionFactory.create("jdbc:unknown"))
+                .isInstanceOf(UncheckedSQLException.class)
+                .hasMessage("Error connecting to 'jdbc:unknown': No suitable driver found for jdbc:unknown");
+    }
+
+    @Test
+    void createConnectionFailsUnknownUrl() {
+        assertThatThrownBy(() -> connectionFactory.create("unknown"))
+                .isInstanceOf(UncheckedSQLException.class)
+                .hasMessage("Error connecting to 'unknown': No suitable driver found for unknown");
+    }
+
+    @Test
+    void createConnectionFailsInvalidJdbcUrl() {
+        assertThatThrownBy(() -> connectionFactory.create("jdbc:h2:unknown")).isInstanceOf(UncheckedSQLException.class)
+                .hasMessageStartingWith(
+                        "Error connecting to 'jdbc:h2:unknown': A file path that is implicitly relative to the current working directory is not allowed in the database URL");
     }
 }
