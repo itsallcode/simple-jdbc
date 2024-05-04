feb472ea9aa8acc2dc39c8ba307d80663247e8e6
kaklakariada <kaklakariada@users.noreply.github.com>
2024-05-04 14:39:32 +0200

    Add tests for result set types

--- src/integrationTest/java/org/itsallcode/jdbc/ExasolTypeTest.java
+++ src/integrationTest/java/org/itsallcode/jdbc/ExasolTypeTest.java
@@ -4,7 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.math.BigDecimal;
-import java.sql.*;
+import java.sql.Date;
+import java.sql.JDBCType;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.util.stream.Stream;
@@ -21,7 +22,7 @@ import com.exasol.containers.ExasolService;
 
 class ExasolTypeTest {
 
-    private static final ExasolContainer<?> container = new ExasolContainer<>("8.26.0")
+    private static final ExasolContainer<?> container = new ExasolContainer<>("8.27.0")
             .withRequiredServices(ExasolService.JDBC).withReuse(true);
 
     @BeforeAll
@@ -49,8 +50,10 @@ class ExasolTypeTest {
             assertAll(
                     () -> assertThat(value.getClass()).isEqualTo(test.expectedValue().getClass()),
                     () -> assertThat(value).isEqualTo(test.expectedValue()),
-                    () -> assertThat(columnValue.type().jdbcType()).as("jdbc type").isEqualTo(test.expectedType()),
-                    () -> assertThat(columnValue.type().typeName()).as("type name").isEqualTo(test.expectedTypeName()),
+                    () -> assertThat(columnValue.type().jdbcType()).as("jdbc type")
+                            .isEqualTo(test.expectedType()),
+                    () -> assertThat(columnValue.type().typeName()).as("type name")
+                            .isEqualTo(test.expectedTypeName()),
                     () -> assertThat(columnValue.type().className()).as("type class name")
                             .isEqualTo(test.expectedClassName()));
         }
@@ -64,8 +67,10 @@ class ExasolTypeTest {
             final ColumnValue value = result.toList().get(0).get(0);
             assertAll(
                     () -> assertThat(value.value()).isNull(),
-                    () -> assertThat(value.type().jdbcType()).isEqualTo(test.expectedType()),
-                    () -> assertThat(value.type().typeName()).isEqualTo(test.expectedTypeName()));
+                    () -> assertThat(value.type().jdbcType()).as("jdbc type").isEqualTo(test.expectedType()),
+                    () -> assertThat(value.type().typeName()).as("type name").isEqualTo(test.expectedTypeName()),
+                    () -> assertThat(value.type().className()).as("type class name")
+                            .isEqualTo(test.expectedClassName()));
         }
     }
 
@@ -87,58 +92,77 @@ class ExasolTypeTest {
     static Stream<Arguments> testTypes() {
         return Stream.of(
                 typeTest("2023-11-25 16:18:46", "timestamp", Instant.parse("2023-11-25T16:18:46.0Z"),
-                        JDBCType.TIMESTAMP, "TIMESTAMP", Timestamp.class),
+                        JDBCType.TIMESTAMP, "TIMESTAMP", "java.sql.TimeStamp"),
                 typeTest("2023-11-25 16:18:46", "timestamp with local time zone",
                         Instant.parse("2023-11-25T16:18:46.0Z"),
-                        JDBCType.TIMESTAMP, "TIMESTAMP", Timestamp.class),
-                typeTest("2023-11-25", "date", LocalDate.parse("2023-11-25"), JDBCType.DATE, "DATE", Date.class),
-                typeTest("5-3", "INTERVAL YEAR TO MONTH", "+05-03", JDBCType.VARCHAR, "INTERVAL YEAR TO MONTH",
+                        JDBCType.TIMESTAMP, "TIMESTAMP", "java.sql.TimeStamp"),
+                typeTest("2023-11-25", "date", LocalDate.parse("2023-11-25"), JDBCType.DATE, "DATE",
+                        Date.class),
+                typeTest("5-3", "INTERVAL YEAR TO MONTH", "+05-03", JDBCType.VARCHAR,
+                        "INTERVAL YEAR TO MONTH",
                         String.class),
-                typeTest("2 12:50:10.123", "INTERVAL DAY TO SECOND", "+02 12:50:10.123", JDBCType.VARCHAR,
+                typeTest("2 12:50:10.123", "INTERVAL DAY TO SECOND", "+02 12:50:10.123",
+                        JDBCType.VARCHAR,
                         "INTERVAL DAY TO SECOND", String.class),
-                typeTest("POINT(1 2)", "GEOMETRY", "POINT (1 2)", JDBCType.VARCHAR, "GEOMETRY", String.class),
-                typeTest("550e8400-e29b-11d4-a716-446655440000", "HASHTYPE", "550e8400e29b11d4a716446655440000",
+                typeTest("POINT(1 2)", "GEOMETRY", "POINT (1 2)", JDBCType.VARCHAR, "GEOMETRY",
+                        String.class),
+                typeTest("550e8400-e29b-11d4-a716-446655440000", "HASHTYPE",
+                        "550e8400e29b11d4a716446655440000",
                         JDBCType.CHAR, "HASHTYPE", String.class),
                 typeTest("text", "VARCHAR(10)", "text", JDBCType.VARCHAR, "VARCHAR", String.class),
                 typeTest("text", "CHAR(10)", "text      ", JDBCType.CHAR, "CHAR", String.class),
                 typeTest("123.456", "DECIMAL", 123L, JDBCType.BIGINT, "BIGINT", Long.class),
-                typeTest("123", "SHORTINT", 123, JDBCType.INTEGER, "INTEGER", Long.class),
-                typeTest("123", "SMALLINT", 123, JDBCType.INTEGER, "INTEGER", Long.class),
-                typeTest("123", "TINYINT", (short) 123, JDBCType.SMALLINT, "SMALLINT", Long.class),
-                typeTest("123", "DECIMAL(4,0)", (short) 123, JDBCType.SMALLINT, "SMALLINT", Long.class),
-                typeTest("123", "DECIMAL(5,0)", 123, JDBCType.INTEGER, "INTEGER", Long.class),
-                typeTest("123", "DECIMAL(9,0)", 123, JDBCType.INTEGER, "INTEGER", Long.class),
+                typeTest("123", "SHORTINT", 123, JDBCType.INTEGER, "INTEGER", Integer.class),
+                typeTest("123", "SMALLINT", 123, JDBCType.INTEGER, "INTEGER", Integer.class),
+                typeTest("123", "TINYINT", (short) 123, JDBCType.SMALLINT, "SMALLINT", Short.class),
+                typeTest("123", "DECIMAL(4,0)", (short) 123, JDBCType.SMALLINT, "SMALLINT", Short.class),
+                typeTest("123", "DECIMAL(5,0)", 123, JDBCType.INTEGER, "INTEGER", Integer.class),
+                typeTest("123", "DECIMAL(9,0)", 123, JDBCType.INTEGER, "INTEGER", Integer.class),
                 typeTest("123", "DECIMAL(10,0)", 123L, JDBCType.BIGINT, "BIGINT", Long.class),
                 typeTest("123", "DECIMAL(18,0)", 123L, JDBCType.BIGINT, "BIGINT", Long.class),
-                typeTest("123", "DECIMAL(19,0)", BigDecimal.valueOf(123), JDBCType.DECIMAL, "DECIMAL", Long.class),
+                typeTest("123", "DECIMAL(19,0)", BigDecimal.valueOf(123), JDBCType.DECIMAL, "DECIMAL",
+                        BigDecimal.class),
                 typeTest("123", "INT", 123L, JDBCType.BIGINT, "BIGINT", Long.class),
-                typeTest("123", "BIGINT", BigDecimal.valueOf(123), JDBCType.DECIMAL, "DECIMAL", Long.class),
+                typeTest("123", "BIGINT", BigDecimal.valueOf(123), JDBCType.DECIMAL, "DECIMAL",
+                        BigDecimal.class),
                 typeTest("123", "DEC", 123L, JDBCType.BIGINT, "BIGINT", Long.class),
                 typeTest("123", "NUMERIC", 123L, JDBCType.BIGINT, "BIGINT", Long.class),
                 typeTest("123.457", "DECIMAL(6,3)", BigDecimal.valueOf(123.457d), JDBCType.DECIMAL, "DECIMAL",
+                        BigDecimal.class),
+                typeTest("123.458", "DOUBLE", 123.458d, JDBCType.DOUBLE, "DOUBLE PRECISION",
                         Double.class),
-                typeTest("123.458", "DOUBLE", 123.458d, JDBCType.DOUBLE, "DOUBLE PRECISION", Double.class),
-                typeTest("123.458", "FLOAT", 123.458d, JDBCType.DOUBLE, "DOUBLE PRECISION", Double.class),
-                typeTest("123.458", "NUMBER", 123.458d, JDBCType.DOUBLE, "DOUBLE PRECISION", Double.class),
-                typeTest("123.458", "REAL", 123.458d, JDBCType.DOUBLE, "DOUBLE PRECISION", Double.class),
-                typeTest("123.458", "DOUBLE PRECISION", 123.458d, JDBCType.DOUBLE, "DOUBLE PRECISION", Double.class),
+                typeTest("123.458", "FLOAT", 123.458d, JDBCType.DOUBLE, "DOUBLE PRECISION",
+                        Double.class),
+                typeTest("123.458", "NUMBER", 123.458d, JDBCType.DOUBLE, "DOUBLE PRECISION",
+                        Double.class),
+                typeTest("123.458", "REAL", 123.458d, JDBCType.DOUBLE, "DOUBLE PRECISION",
+                        Double.class),
+                typeTest("123.458", "DOUBLE PRECISION", 123.458d, JDBCType.DOUBLE, "DOUBLE PRECISION",
+                        Double.class),
                 typeTest("true", "BOOLEAN", true, JDBCType.BOOLEAN, "BOOLEAN", Boolean.class));
     }
 
     @Test
-    void countStar() {
+    void countStarResultType() {
         try (final SimpleConnection connection = connect();
                 final SimpleResultSet<Row> result = connection
                         .query("select count(*) from (select 1 from dual)")) {
             final Row row = result.toList().get(0);
             final ColumnMetaData columnMetaData = row.columns().get(0);
+            assertThat(columnMetaData.type().jdbcType()).isEqualTo(JDBCType.BIGINT);
         }
     }
 
     private static Arguments typeTest(final String value, final String type, final Object expectedValue,
             final JDBCType expectedType, final String expectedTypeName, final Class<?> expectedClass) {
+        return typeTest(value, type, expectedValue, expectedType, expectedTypeName, expectedClass.getName());
+    }
+
+    private static Arguments typeTest(final String value, final String type, final Object expectedValue,
+            final JDBCType expectedType, final String expectedTypeName, final String expectedClassName) {
         return Arguments
-                .of(new TypeTest(value, type, expectedValue, expectedType, expectedTypeName, expectedClass.getName()));
+                .of(new TypeTest(value, type, expectedValue, expectedType, expectedTypeName,
+                        expectedClassName));
     }
 
     record TypeTest(String value, String type, Object expectedValue, JDBCType expectedType, String expectedTypeName,
