56ae0f0978d8d36045ad09788a0fcd1c76d1c64d
kaklakariada <kaklakariada@users.noreply.github.com>
2024-12-21 14:42:58 +0100

    Check operation preconditions

--- src/main/java/org/itsallcode/jdbc/Transaction.java
+++ src/main/java/org/itsallcode/jdbc/Transaction.java
@@ -11,8 +11,11 @@ import org.itsallcode.jdbc.resultset.generic.Row;
 public final class Transaction implements DbOperations {
 
     private final SimpleConnection connection;
+    private boolean closed;
+    private boolean committed;
+    private boolean rolledBack;
 
-    private Transaction(final SimpleConnection connection) {
+    Transaction(final SimpleConnection connection) {
         this.connection = connection;
     }
 
@@ -23,62 +26,97 @@ public final class Transaction implements DbOperations {
 
     /**
      * Commit the transaction.
+     * <p>
+     * No further operations are allowed on this transaction afterwards.
      */
     public void commit() {
+        checkOperationAllowed();
         connection.commit();
+        this.committed = true;
     }
 
     /**
      * Rollback the transaction.
+     * <p>
+     * No further operations are allowed on this transaction afterwards.
      */
     public void rollback() {
+        checkOperationAllowed();
         connection.rollback();
+        this.rolledBack = true;
     }
 
     @Override
     public void executeStatement(final String sql) {
+        checkOperationAllowed();
         connection.executeStatement(sql);
     }
 
     @Override
     public void executeStatement(final String sql, final PreparedStatementSetter preparedStatementSetter) {
+        checkOperationAllowed();
         connection.executeStatement(sql, preparedStatementSetter);
     }
 
     @Override
     public SimpleResultSet<Row> query(final String sql) {
+        checkOperationAllowed();
         return connection.query(sql);
     }
 
     @Override
     public void executeScript(final String sqlScript) {
+        checkOperationAllowed();
         connection.executeScript(sqlScript);
     }
 
     @Override
     public <T> SimpleResultSet<T> query(final String sql, final RowMapper<T> rowMapper) {
+        checkOperationAllowed();
         return connection.query(sql, rowMapper);
     }
 
     @Override
     public <T> SimpleResultSet<T> query(final String sql, final PreparedStatementSetter preparedStatementSetter,
             final RowMapper<T> rowMapper) {
+        checkOperationAllowed();
         return connection.query(sql, preparedStatementSetter, rowMapper);
     }
 
     @Override
     public <T> BatchInsertBuilder<T> batchInsert(final Class<T> rowType) {
+        checkOperationAllowed();
         return connection.batchInsert(rowType);
+    }
+
+    private void checkOperationAllowed() {
+        if (this.closed) {
+            throw new IllegalStateException("Operation not allowed on closed transaction");
+        }
+        if (this.rolledBack) {
+            throw new IllegalStateException("Operation not allowed on rolled back transaction");
+        }
+        if (this.committed) {
+            throw new IllegalStateException("Operation not allowed on committed transaction");
+        }
     }
 
     /**
      * Rollback transaction and enable auto commit.
      * <p>
      * Explicitly run {@link #commit()} before to commit your transaction.
+     * <p>
+     * No further operations are allowed on this transaction afterwards.
      */
     @Override
     public void close() {
-        this.rollback();
+        if (closed) {
+            return;
+        }
+        if (!rolledBack && !committed) {
+            this.rollback();
+        }
         connection.setAutoCommit(true);
+        this.closed = true;
     }
 }
